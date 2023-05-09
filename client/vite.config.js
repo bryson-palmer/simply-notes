5a43932efa7f8de851f1f71291da76aa759bb10e
Lance Kindle <lance.kindle@gmail.com>
2023-05-09 16:41:07 -0700

    creating alias config for '@' to reach root-level (rather than using ../.. for relative pathing)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import url from 'url'
+import path from 'path'
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, 'src') }]
+  }
 })
