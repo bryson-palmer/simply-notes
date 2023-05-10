78d652a809cbde0669edf02d918e9cbbc9b3931c
Lance Kindle <lance.kindle@gmail.com>
2023-05-10 16:53:20 -0700

    setup theme files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useMemo } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box, CssBaseline, ThemeProvider, Typography } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
+import { themeSettings } from '@/theme'
 
 function App() {
-  // setCount will update constant count, and return it's value
-  const [count, setCount] = useState(0)
-  const handleIncrement = () => {
-    return setCount(count + 1);
-  }
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleIncrement}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+
+    </div>
   )
 }
 
