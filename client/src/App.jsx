638bac9361aa4361ab0fc469f80bdc3673fb280b
Lance Kindle <lance.kindle@gmail.com>
2023-05-10 16:39:09 -0700

    exploring modifying increments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  // setCount will update constant count, and return it's value
   const [count, setCount] = useState(0)
+  const handleIncrement = () => {
+    return setCount(count + 1);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={handleIncrement}>
           count is {count}
         </button>
         <p>
