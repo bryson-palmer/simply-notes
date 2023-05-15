d3c0699cefd7cc0ea0558d97ef813af04ec0205f
Bryson Palmer <brysonpalmer@gmail.com>
2023-05-15 08:00:57 -0700

    Creating an api layer for the note entity.

--- client/src/apis/noteAPI.js
+++ client/src/apis/noteAPI.js
@@ -2,6 +2,15 @@ import { api } from "./configs/axiosConfigs"
 import { defineCancelApiObject } from "./configs/axiosUtils"
 
 export const noteAPI = {
+  getAll: async (cancel = false) => {
+    const response = await api.request({
+      url: "/notes/",
+      method: "GET",
+      signal: cancel ? cancelApiObject[this.getAll.name].handleRequestCancellation().signal : undefined,
+    })
+
+    return response.data
+  },
   get: async (id, cancel = false) => {
     const response = await api.request({
       url: `/notes/${id}`,
@@ -11,59 +20,52 @@ export const noteAPI = {
     })
 
     // returning the note returned by the API
-    return response.data.note
-  },
-  getAll: async (cancel = false) => {
-    const response = await api.request({
-      url: "/notes/",
-      method: "GET",
-      signal: cancel ? cancelApiObject[this.getAll.name].handleRequestCancellation().signal : undefined,
-    })
-
-    return response.data.notes
-  },
-  search: async (title, cancel = false) => {
-    const response = await api.request({
-      url: "/notes/search",
-      method: "GET",
-      params: {
-        title: title,
-      },
-      signal: cancel ? cancelApiObject[this.search.name].handleRequestCancellation().signal : undefined,
-    })
-
-    return response.data.notes
+    return response.data
   },
   create: async (note, cancel = false) => {
-    await api.request({
+    const response = await api.request({
       url: `/notes/`,
       method: "POST",
       data: note,
       signal: cancel ? cancelApiObject[this.create.name].handleRequestCancellation().signal : undefined,
     })
+
+    return response.data
   },
+  // deleteAll: async (cancel = false) => {
+  //   await api.request({
+  //     url: `/notes`,
+  //     method: "DELETE",
+  //     signal: cancel ? cancelApiObject[this.deleteAll.name].handleRequestCancellation().signal : undefined,
+  //   })
+  // },
   delete: async (id, cancel = false) => {
     await api.request({
       url: `/notes/${id}`,
       method: "DELETE",
-      signal: cancel ? cancelApiObject[this.create.name].handleRequestCancellation().signal : undefined,
+      signal: cancel ? cancelApiObject[this.delete.name].handleRequestCancellation().signal : undefined,
     })
   },
-  deleteAll: async (cancel = false) => {
-    await api.request({
-      url: `/notes/`,
-      method: "DELETE",
-      signal: cancel ? cancelApiObject[this.create.name].handleRequestCancellation().signal : undefined,
-    })
-  },
-  update: async (id, note, cancel = false) => {
-    await api.request({
-      url: `/notes/${id}`,
-      method: "PUT",
-      data: note,
-      signal: cancel ? cancelApiObject[this.create.name].handleRequestCancellation().signal : undefined,
-    })
-  },
+  // update: async (id, note, cancel = false) => {
+  //   await api.request({
+  //     url: `/notes/${id}`,
+  //     method: "PUT",
+  //     data: note,
+  //     signal: cancel ? cancelApiObject[this.update.name].handleRequestCancellation().signal : undefined,
+  //   })
+  // },
+  // search: async (title, cancel = false) => {
+  //   const response = await api.request({
+  //     url: "/notes/search",
+  //     method: "GET",
+  //     params: {
+  //       title: title,
+  //     },
+  //     signal: cancel ? cancelApiObject[this.search.name].handleRequestCancellation().signal : undefined,
+  //   })
+
+  //   return response.data.notes
+  // },
 }
 
 // defining the cancel API object for ProductAPI
