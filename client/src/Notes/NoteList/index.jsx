678e42c2750e7b715c04bdf449c317867377fddd
Bryson Palmer <brysonpalmer@gmail.com>
2023-05-15 08:07:00 -0700

    Extracting Note List Header from NoteList and misc. changes.

--- client/src/Notes/NoteList/index.jsx
+++ client/src/Notes/NoteList/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { PropTypes } from 'prop-types/prop-types'
 
 import { useTheme } from '@emotion/react'
@@ -12,16 +12,19 @@ import {
   ListItemText,
   Typography
 } from '@mui/material'
-import DeleteIcon from '@mui/icons-material/Delete'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
 
-const NoteList = ({
-  notes, 
-  removeNote,
-  setAddNote,
-  setSelectedNote
-}) => {
+import ListHeader from '@/Notes/ListHeader'
+import { useDeleteNote, useGetNote, useNotes } from '@/store/store-selectors'
+
+const NoteList = React.memo(({ setAddNote }) => {
   const [checked, setChecked] = useState([])
+
   const { palette } = useTheme()
+  const notes = useNotes()
+  const getNote = useGetNote()
+  const deleteNote = useDeleteNote()
+  console.log('🚀 ~ file: index.jsx:27 ~ notes:', notes)
   
   const handleCheckToggle = useCallback(value => () => {
     const currentIndex = checked.indexOf(value)
@@ -35,124 +38,117 @@ const NoteList = ({
   }, [checked])
 
   const handleSelectNote = useCallback(value => () => {
-    const selectedNote = notes.find(note => note.id === value)
-
     setAddNote(false)
-    setSelectedNote(selectedNote)
-  }, [notes, setAddNote, setSelectedNote])
+    getNote(value)
+  }, [getNote, setAddNote])
 
-  const handleDeleteNote = useCallback(() => {
-    if (!Array.isArray(checked) || checked.length < 1) return
-    removeNote(checked)
-  }, [checked, removeNote])
+  const handleDeleteNote = useCallback(id => {
+    deleteNote(id)
+  }, [deleteNote])
 
   return (
-    <List
-      sx={{
-        width: "30%",
+    <div
+      style={{
+        width: '30%',
         minWidth: 200,
-        height: "80vh",
+        height: '75vh',
+        overflowY: 'auto',
+        overflowX: 'hidden',
         paddingTop: 0,
-        paddingRight: "1rem",
-        bgcolor: "transparent",
+        paddingRight: '1rem',
+        bgcolor: 'transparent',
         borderRight: `1px solid ${palette.grey[800]}`,
       }}
     >
-      {notes.map(({ id, title, body }) => {
-        const labelId = `notes-list-label-${id}`
+      <ListHeader checked={checked} setChecked={setChecked} />
+      <List>
+        {notes.map(({ id, title, body }) => {
+          const labelId = `notes-list-label-${id}`
 
-        return (
-          <ListItem
-            dense
-            disablePadding
-            key={labelId}
-            sx={{
-              borderRadius: "1rem",
-              paddingLeft: "1rem",
-              "&:hover": { backgroundColor: palette.background.light },
-            }}
-            secondaryAction={
-              <IconButton
-                disableRipple
-                onClick={handleDeleteNote}
-                aria-label="comments"
-                edge="end"
-                sx={{
-                  color: palette.grey[300],
-                  "&:hover": { color: palette.primary[200] },
-                }}
-              >
-                <DeleteIcon />
-              </IconButton>
-            }
-          >
-            <IconButton
-              disableRipple
-              size="small"
-              onClick={handleCheckToggle(id)}
-            >
-              <ListItemIcon
-                sx={{ "&.MuiListItemIcon-root": { minWidth: "auto" } }}
-              >
-                <Checkbox
+          return (
+            <ListItem
+              dense
+              disablePadding
+              key={labelId}
+              sx={{
+                borderRadius: '1rem',
+                paddingLeft: '1rem',
+                '&:hover': { backgroundColor: palette.background.light },
+              }}
+              secondaryAction={
+                <IconButton
                   disableRipple
-                  edge="start"
-                  checked={checked.includes(id)}
-                  tabIndex={-1}
-                  inputProps={{ "aria-labelledby": labelId }}
+                  onClick={() => handleDeleteNote(id)}
+                  aria-label={`delete-note -${id}`}
+                  edge='end'
                   sx={{
                     color: palette.grey[300],
-                    "&:hover": { color: palette.primary[200] },
+                    '&:hover': { color: palette.primary[200] },
                   }}
-                />
-              </ListItemIcon>
-            </IconButton>
-            <ListItemButton
-              role={undefined}
-              onClick={handleSelectNote(id)}
-              sx={{ "&:hover": { backgroundColor: "transparent" } }}
+                >
+                  <DeleteForeverIcon />
+                </IconButton>
+              }
             >
-              <ListItemText
-                id={labelId}
-                sx={{
-                  color: palette.grey[200],
-                  "&:hover": { color: palette.primary[200] },
-                }}
-                primary={title}
-                primaryTypographyProps={{ noWrap: true }}
-                secondary={
-                  <Typography
-                    noWrap
-                    variant="h5"
+              <IconButton
+                disableRipple
+                size='small'
+                onClick={handleCheckToggle(id)}
+              >
+                <ListItemIcon
+                  sx={{ '&.MuiListItemIcon-root': { minWidth: 'auto' } }}
+                >
+                  <Checkbox
+                    disableRipple
+                    edge='start'
+                    checked={checked.includes(id)}
+                    tabIndex={-1}
+                    inputProps={{ 'aria-labelledby': labelId }}
                     sx={{
-                      color: palette.grey[600],
-                      display: { sm: "none", md: "block" },
+                      color: palette.grey[300],
+                      '&:hover': { color: palette.primary[200] },
                     }}
-                  >
-                    {body}
-                  </Typography>
-                }
-              />
-            </ListItemButton>
-          </ListItem>
-        )
-      })}
-    </List>
+                  />
+                </ListItemIcon>
+              </IconButton>
+              <ListItemButton
+                role={undefined}
+                onClick={handleSelectNote(id)}
+                sx={{ '&:hover': { backgroundColor: 'transparent' } }}
+              >
+                <ListItemText
+                  id={labelId}
+                  sx={{
+                    color: palette.grey[200],
+                    '&:hover': { color: palette.primary[200] },
+                  }}
+                  primary={title}
+                  primaryTypographyProps={{ noWrap: true }}
+                  secondary={
+                    <Typography
+                      noWrap
+                      variant='h5'
+                      sx={{
+                        color: palette.grey[600],
+                        display: { sm: 'none', md: 'block' },
+                      }}
+                    >
+                      {body}
+                    </Typography>
+                  }
+                />
+              </ListItemButton>
+            </ListItem>
+          )
+        })}
+      </List>
+    </div>
   )
-}
+})
 
 NoteList.displayName = '/NoteList'
 NoteList.propTypes = {
-  notes: PropTypes.arrayOf(PropTypes.shape({
-    note: PropTypes.shape({
-      id: PropTypes.string,
-      title: PropTypes.string,
-      body: PropTypes.string
-    })
-  })),
-  removeNote: PropTypes.func,
   setAddNote: PropTypes.func,
-  setSelectedNote: PropTypes.func
 }
 
 export default NoteList
