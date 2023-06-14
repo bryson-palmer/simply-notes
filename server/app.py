1c545db3b39bac5c46e548ea9e52d24ed8033167
Lance Kindle <lance.kindle@gmail.com>
2023-06-14 10:23:17 -0700

    update NOTES table to include folder

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ connection = sqlite3.connect(db_file)
 
 cursor = connection.cursor()
 create_notes_table = """
-  CREATE TABLE IF NOT EXISTS NOTES(id, title, body, user_id)
+  CREATE TABLE IF NOT EXISTS NOTES(id, title, body, user_id, folder_id)
 """
 create_folders_table = """
   CREATE TABLE IF NOT EXISTS FOLDERS(id, folderName)
@@ -24,6 +24,19 @@ create_folders_table = """
 cursor.execute(create_notes_table)
 cursor.execute(create_folders_table)
 connection.commit()
+
+# adding folder_id to NOTES existing NOTES table
+cursor.execute('PRAGMA user_version')
+result = cursor.fetchone()
+version = result[0]
+print(version)
+if version == 0:
+   add_folder_to_notes_table = """
+     ALTER TABLE NOTES ADD COLUMN folder_id
+   """
+   cursor.execute(add_folder_to_notes_table)
+   cursor.execute(f'PRAGMA user_version = {version + 1}')
+   connection.commit()
 
 @app.route('/')
 def home():
