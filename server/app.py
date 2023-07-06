c317e4c9c8df05313a960772049dfc62c4e6749e
Bryson Palmer <brysonpalmer@gmail.com>
2023-07-06 12:09:17 -0700

    Setting session cookie for samesite and secure for chrome.

--- server/app.py
+++ server/app.py
@@ -10,6 +10,9 @@ from pprint import pprint
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 app.permanent_session_lifetime = timedelta(days=31)  # how long before they must log in again
+# Work around for cookies not saving in Chrome
+app.config["SESSION_COOKIE_SAMESITE"] = "None"
+app.config["SESSION_COOKIE_SECURE"] = "True"
 
 # load secret key from .env file (or create it and then load it)
 SECRET_FILE = '.env.local'
