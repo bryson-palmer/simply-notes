78be9d68f3571f11d5b2620e6e32cbec964ec8b8
Bryson Palmer <brysonpalmer@gmail.com>
2023-07-11 11:20:20 -0700

    Moved users code to own file.

---
+++ server/users.py
@@ -0,0 +1,29 @@
+import sqlite3
+import uuid
+
+from constants import DB_FILE
+
+
+def get_new_user_id():
+    connection = sqlite3.connect(DB_FILE)
+    cursor = connection.cursor()
+    already_exists = True
+    while already_exists:
+        id = uuid.uuid4().hex
+        cursor.execute('SELECT * FROM USERS WHERE id="%s"' % id)
+        already_exists = cursor.fetchone()
+    # only fill in ID
+    cursor.execute('INSERT INTO USERS (id) VALUES ("%s")' % id)
+    connection.commit()
+    connection.close()
+    return id
+
+
+def create_new_user_if_uninitialized(session):
+    if session.get('user_id'):
+        return
+    user_id = get_new_user_id()
+    session['user_id'] = user_id
+    session.permanent = True
+    session.modified = True
+    # also handle copying demo data from demo user to this new user's data
