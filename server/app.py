0f0273e1db8acc9aaa7f629498febff69f6227e3
Bryson Palmer <brysonpalmer@gmail.com>
2023-07-11 11:19:22 -0700

    Refactoring app.py and moving folders and users to their own files.

--- server/app.py
+++ server/app.py
@@ -1,15 +1,8 @@
-import sqlite3
-from datetime import timedelta
-import uuid
-from flask import Flask, request, session
-import json
-from flask_cors import CORS
-import os
-from pprint import pprint
-from constants import DB_FILE
 import db_setup
-from app_setup import app, app_configure
+import folders
 import notes  # this auto-decoreates note-related functions with app.route(...)
+from app_setup import app, app_configure
+from users import create_new_user_if_uninitialized
 
 app_configure()  # set up secret keys, cookie settings, CORS, etc.
 
@@ -18,89 +11,5 @@ db_setup.create_update_tables()
 @app.route('/')
 def home():
 	return 'Flask app is running!!!'
-
-def get_new_user_id():
-  connection = sqlite3.connect(DB_FILE)
-  cursor = connection.cursor()
-  already_exists = True
-  while already_exists:
-    id = uuid.uuid4().hex
-    cursor.execute('SELECT * FROM USERS WHERE id="%s"' % id)
-    already_exists = cursor.fetchone()
-  # only fill in ID
-  cursor.execute('INSERT INTO USERS (id) VALUES ("%s")' % id)
-  connection.commit()
-  connection.close()
-  return id
-
-def create_new_user_if_uninitialized(session):
-  print(session.get('user_id'))
-  if session.get('user_id'):
-    return
-  user_id = get_new_user_id()
-  session['user_id'] = user_id
-  session.permanent = True
-  session.modified = True
-  # also handle copying demo data from demo user to this new user's data
-
-@app.route('/folders', methods=['GET', 'POST'])
-def folders():
-    # this is the first thing the app loads on startup. If this is a user who hasn't logged in yet,
-    # give them demo data (but how?)
-    create_new_user_if_uninitialized(session)
-    user_id = session.get('user_id')
-    print(f'folders: user_id: {user_id}')
-    connection = sqlite3.connect(DB_FILE)
-    cursor = connection.cursor()
-    if request.method == 'GET':
-        cursor.execute('SELECT * FROM FOLDERS WHERE user_id="%s"' % user_id)
-        results = cursor.fetchall()
-        print('folders *', results)
-
-        folders=[]
-        for result in results:
-          (id, folderName, _) = result  # _ is user_id, we don't include that data!!!
-          folder = dict(id=id, folderName=folderName)
-          folders.append(folder)
-        
-        connection.close()
-        return folders
-    
-    if request.method == 'POST':
-        folder = request.json
-        is_new_folder = False
-        folder_name = folder['folderName']
-        id = folder.get('id')
-
-        if id is None or id == '':
-            id = uuid.uuid4().hex # a 32-character lowercase hexadecimal string
-            folder['id'] = id
-            is_new_folder = True
-
-        if is_new_folder:
-            cursor.execute(f'INSERT INTO FOLDERS (id, folderName, user_id) VALUES ("{id}", "{folder_name}", "{user_id}")')
-        if not is_new_folder:
-            cursor.execute('UPDATE FOLDERS SET folderName="%s" where id="%s" and user_id="%s"' % (folder_name, id, user_id))
-        connection.commit()
-        connection.close()
-
-        return id
-
-@app.route('/folders/<id>', methods=['DELETE'])
-def folder_delete(id):
-  tuple_ids = tuple(id.split(','))
-
-  connection = sqlite3.connect(DB_FILE)
-  cursor = connection.cursor()
-
-  if len(tuple_ids) == 1:
-    cursor.execute(f'DELETE FROM FOLDERS WHERE id = "{tuple_ids[0]}"')
-  else:
-    cursor.execute(f'DELETE FROM FOLDERS WHERE id IN {tuple_ids}')
-  
-  connection.commit()
-  connection.close()
-
-  return 'Delete success'
 
 app.run(debug=True)
