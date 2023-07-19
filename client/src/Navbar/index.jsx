f0ba49544504659a17913bfa69813e182819935f
Bryson Palmer <brysonpalmer@gmail.com>
2023-07-19 15:46:04 -0700

    Navbar revision. Changed logo to home button. Moved addNote button here as well.

--- client/src/Navbar/index.jsx
+++ client/src/Navbar/index.jsx
@@ -1,60 +1,85 @@
-import { useState } from 'react'
-
+import { useCallback } from 'react'
+import { PropTypes } from 'prop-types/prop-types'
 import { Link } from 'react-router-dom'
 
-import PixIcon from '@mui/icons-material/Pix'
-import { Box, Typography, useTheme } from '@mui/material'
+import { Add as AddIcon, Create as CreateIcon, Pix as PixIcon } from '@mui/icons-material'
+import { Button, Fade, IconButton,  useTheme } from '@mui/material'
 
+import { useFolders } from '@/store/store-selectors'
 import FlexBetween from '@/ui/FlexBetween'
 import StyledTooltip from '@/ui/StyledTooltip'
 
-const Navbar = () => {
-  const [selected, setSelected] = useState('notes')
+const Navbar = ({ setIsNewNote }) => {
   const { palette } = useTheme()
+  const folders = useFolders()
+
+  const handleIsNewNote = useCallback(() => {
+    setIsNewNote(true)
+  }, [setIsNewNote])
 
   return (
     <FlexBetween
+      m='0 -4px'
       mb='0.25rem'
       p='0.5rem 0rem'
       color={palette.grey[300]}
     >
       {/* Left Side */}
-      <FlexBetween gap='0.75rem'>
-        <PixIcon sx={{ fontSize: '28px' }} />
-        <Typography variant='h4' fontSize='16px'>
-          ToDo
-        </Typography>
-      </FlexBetween>
+      <Link to='/'>
+        <Button
+          startIcon={<PixIcon />}
+          sx={{
+            color: palette.secondary[400],
+            fontSize: { xs: '1.2rem', sm: '1.2rem', md: '1.5rem' },
+            fontWeight: 600,
+            textTransform: 'none',
+            lineHeight: 1.2,
+            '& [class*=MuiButton-startIcon]': {
+              marginRight: '6px',
+              '& > svg': {
+                fontSize: { xs: '1.5rem', sm: '1.5rem', md: '2rem' },
+              },
+            }
+          }}
+        >
+          Simple-Notes
+        </Button>
+      </Link>
 
       {/* Right Side */}
-      <FlexBetween gap='2rem'>
-        <Box sx={{ '&:hover': { color: palette.primary[100] } }}>
-          <Link
-            to='/'
-            onClick={() => setSelected('notes')}
-            style={{
-              color: selected === 'notes' ? 'inherit' : palette.grey[700],
-              textDecoration: 'inherit'
-            }}
+      <FlexBetween gap='1rem'>
+        <IconButton
+          disabled={!folders.length}
+          onClick={handleIsNewNote}
+          sx={{
+            color: palette.secondary[400],
+            '&:hover': { color: palette.secondary[100] },
+            '&.Mui-disabled': { color: palette.secondary[400], opacity: 0.5},
+            '& [class*=MuiSvgIcon-root]': {
+              fontSize: { xs: '1.2rem', sm: '1.2rem', md: '1.5rem' }
+            }
+          }}
+        >
+          <StyledTooltip
+            arrow
+            title='New note'
+            TransitionComponent={Fade}
+            TransitionProps={{ timeout: 400 }}
           >
-            notes
-          </Link>
-        </Box>
-        <Box sx={{ '&:hover': { color: palette.primary[100] } }}>
-          <Link
-            to='/chat'
-            onClick={() => setSelected('chat')}
-            style={{
-              color: selected === 'chat' ? 'inherit' : palette.grey[700],
-              textDecoration: 'inherit'
-            }}
-          >
-            chat
-          </Link>
-        </Box>
+            <>
+              <AddIcon sx={{ marginRight: '-4px' }} />
+              <CreateIcon />
+            </>
+          </StyledTooltip>
+        </IconButton>
       </FlexBetween>
     </FlexBetween>
   )
+}
+
+Navbar.displayName = 'Navbar'
+Navbar.propTypes = {
+  setIsNewNote: PropTypes.func
 }
 
 export default Navbar
