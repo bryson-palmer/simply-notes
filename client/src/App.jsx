b57f3b63461c724488962468b744367713088206
Bryson Palmer <brysonpalmer@gmail.com>
2023-07-20 10:39:39 -0700

    Removing isNewNote state from App since putting it in the store.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,15 @@
-import { useMemo, useState } from 'react'
-
+import { useMemo } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Box, CssBaseline, ThemeProvider, Typography } from '@mui/material'
 import { createTheme } from '@mui/material/styles'
 
-import StoreContextProvider from '@/store/store-provider'
 import Navbar from '@/Navbar'
 import Notes from '@/Notes'
+import StoreContextProvider from '@/store/store-provider'
 import { themeSettings } from '@/theme'
 
 const App = () => {
-  const [isNewNote, setIsNewNote] = useState(false)
   const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
@@ -22,12 +20,12 @@ const App = () => {
               <CssBaseline />
 
               <Box width='100%' height='100%' padding='1rem 2rem 4rem 2rem'>
-                <Navbar setIsNewNote={setIsNewNote} />
+                <Navbar />
                 <Routes>
-                  <Route index element={<Notes isNewNote={isNewNote} setIsNewNote={setIsNewNote} />} />
+                  <Route index element={<Notes />} />
                   <Route
                     path='notes'
-                    element={<Notes isNewNote={isNewNote} setIsNewNote={setIsNewNote} />}
+                    element={<Notes />}
                   />
                   <Route path='chat' element={<Typography variant="h2" color={theme.palette.secondary[400]}>Lets chat</Typography>} />
                   {/* <Route path='*' element={<404 />} /> */}
