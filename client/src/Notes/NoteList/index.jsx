31cbe996e00171d2a68a6467f0e3d28144e40c13
Bryson Palmer <brysonpalmer@gmail.com>
2023-07-21 22:23:14 -0700

    NoteList styling and refactoring.

--- client/src/Notes/NoteList/index.jsx
+++ client/src/Notes/NoteList/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { PropTypes } from 'prop-types/prop-types'
 
 import { useTheme } from '@emotion/react'
@@ -15,9 +15,17 @@ import {
 import { DeleteForever as DeleteForeverIcon, Description as DescriptionIcon } from '@mui/icons-material'
 
 import ListHeader from '@/Notes/ListHeader'
+import {
+  useDeleteNote,
+  useGetNote,
+  useNotes,
+  useScreenSize,
+  useSelectedNote,
+  useSetIsNewNote
+} from '@/store/store-selectors'
 import EmptyState from '@/ui/EmptyState'
 
-const NoteList = React.memo(({ setIsNewNote }) => {
+const NoteList = React.memo(() => {
   const [listState, setListState] = useState({
     isAllChecked: false,
     checkedIds: []
@@ -25,9 +33,17 @@ const NoteList = React.memo(({ setIsNewNote }) => {
 
   const { palette } = useTheme()
   const notes = useNotes()
+  const screenSize = useScreenSize()
   const selectedNote = useSelectedNote()
+  const setIsNewNote = useSetIsNewNote()
   const getNote = useGetNote()
   const deleteNote = useDeleteNote()
+
+  const notesListWidth = useMemo(() => {
+    if (screenSize === 'large') return 350
+    if (screenSize === 'tablet') return 300
+    if (screenSize === 'desktop' || screenSize === 'mobile') return 224
+  }, [screenSize])
 
   const Icon = () => <DescriptionIcon />
   
@@ -61,18 +77,19 @@ const NoteList = React.memo(({ setIsNewNote }) => {
   return (
     <div
       style={{
-        width: 'clamp(225px, 30%, 300px)',
-        overflowY: 'auto',
-        overflowX: 'hidden',
-        paddingTop: 0,
-        paddingRight: '0.5rem',
-        bgcolor: 'transparent',
-        borderRight: `1px solid ${palette.grey[800]}`,
+        width: notesListWidth,
+        paddingTop: '0.5rem',
       }}
     >
       <ListHeader listState={listState} setListState={setListState} />
       {notes.length ? (
-      <List>
+        <List sx={{
+          height: '88vh',
+          overflow: 'auto',
+          paddingRight: '0.5rem',
+          marginLeft: '0.5rem',
+          borderRight: `thin solid ${palette.grey[900]}`,
+        }}>
         {notes.map(({ id, title, body }) => {
           const labelId = `notes-list-label-${id}`
 
