7a50ee9f49f7e3b7f537266be6bd0bfc2f0ac78b
Lance Kindle <lance.kindle@gmail.com>
2023-07-25 13:37:12 -0700

    wip try to load frontend from backend root

--- server/app.py
+++ server/app.py
@@ -13,8 +13,31 @@ else:
 db_setup.create_update_tables()
 
 @app.route('/')
-def home():
-	return 'Flask app is running!!!'
+def home():  # when
+	''' when navigating here, redirect user to load resources from front-end server.
+		This response is exactly the first thing loaded when querying the frontend root.
+	'''
+	react_html_root = '''
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<link rel="apple-touch-icon" sizes="180x180" href="https://simple-notes-gamma.vercel.app/apple-touch-icon.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="https://simple-notes-gamma.vercel.app/favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="https://simple-notes-gamma.vercel.app/favicon-16x16.png">
+			<link rel="manifest" href="https://simple-notes-gamma.vercel.app/site.webmanifest">
+			<title>ToDo</title>
+			<script type="module" crossorigin src="https://simple-notes-gamma.vercel.app/assets/index.js"></script>
+			<link rel="stylesheet" href="https://simple-notes-gamma.vercel.app/assets/index.css">
+		</head>
+		<body>
+			<div id="root"></div>
+			
+		</body>
+		</html>
+	'''
+	return react_html_root
 
 if __name__ == '__main__':
 	app.run(debug=True)
