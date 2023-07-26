ef967e24774ab361104a14c66d7966810c869419
Lance Kindle <lance.kindle@gmail.com>
2023-07-26 13:54:26 -0700

    add persistent db storage
    with fallback to use the standard app.db file if running locally

--- server/constants.py
+++ server/constants.py
@@ -1,5 +1,14 @@
 import os
+import sqlite3
 
 DIRNAME = os.path.dirname(__file__)  # removes filename from path to just get directory
 FILENAME = os.path.join(DIRNAME, '../client/mockData/db.json')
-DB_FILE = os.path.join(DIRNAME, 'app.db')
+DB_FILE = os.path.join('/data', 'app.db')
+
+# we need to verify DB_FILE works as expected, or default back to original implementation
+try:
+    connection = sqlite3.connect(DB_FILE)
+    connection.close()
+except sqlite3.OperationalError:
+    print('no /data volume! DB_FILE fallback')
+    DB_FILE = os.path.join(DIRNAME, 'app.db')
