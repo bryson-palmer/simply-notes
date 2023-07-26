890a3c8cea0eb3371b904598209d2f182004cfc0
Lance Kindle <lance.kindle@gmail.com>
2023-07-26 11:54:10 -0700

    test if deploying wipes db data

--- server/app_setup.py
+++ server/app_setup.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def app_configure():
     CORS(app, supports_credentials=True)
     app.permanent_session_lifetime = timedelta(days=31)  # how long before they must log in again
-    # Work around for cookies not saving in Chrome
+    # Work around for cookies not saving in Chrome 
     app.config["SESSION_COOKIE_SAMESITE"] = "None"
     # with None, cookies are less secure, can be sent as 3rd party cookies. Safari doesn't allow them.
     # Workaround for safari is to actually bootstrap the front-end from the backend root
