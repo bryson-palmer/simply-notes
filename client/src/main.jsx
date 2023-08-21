ffe62a92f81a03492a8eab9170056f0b0bfabf6f
Bryson Palmer <brysonpalmer@gmail.com>
2023-08-21 14:35:20 -0700

    New react query default options in main jsx file.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,16 +5,20 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App'
 import './index.css'
 
-const queryClient = new QueryClient()
-// Here we can pass in some default values
-// {
-//   defaultOptions: {
-//     queries: {
-//       // refetchOnWindowFocus: false,
-//       // staleTime: 5000
-//     }
-//   }
-// }
+const queryClient = new QueryClient(
+  {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+        retry: false,
+        staleTime: Infinity,
+        cacheTime: Infinity,
+      }
+    }
+  }
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
