31844b974345430d5540afae885f8d82962f4238
Bryson Palmer <brysonpalmer@gmail.com>
2023-08-21 15:47:09 -0700

    Preventing a fetch on note when it's a new note and waiting until it's not a new note.

--- client/src/hooks/useGetNote.js
+++ client/src/hooks/useGetNote.js
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/react-query'
 
 import { noteAPI } from '@/apis/noteAPI'
+import { useStore } from '@/store/store'
 
 export default function useGetNote(noteId) {
+  const isNewNote = useStore(store => store.isNewNote)
   return useQuery({
     queryKey: ['notes', noteId],
     queryFn: () => noteAPI.get(noteId),
-    enabled: Boolean(noteId),
+    enabled: Boolean(noteId && !isNewNote),
     // keepPreviousData: true,
   })
 }
