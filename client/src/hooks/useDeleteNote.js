07e9b818e75ab54f45b6bb055bfe154327d9e42f
Bryson Palmer <brysonpalmer@gmail.com>
2023-09-11 13:08:59 -0700

    Handle view transition animate-out.

--- client/src/hooks/useDeleteNote.js
+++ client/src/hooks/useDeleteNote.js
@@ -5,7 +5,21 @@ import { noteAPI } from '@/apis/noteAPI'
 export default function useDeleteNote() {
   const queryClient = useQueryClient()
   return useMutation({
-    mutationFn: noteId => noteAPI.delete(noteId),
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['notes'] })
+    mutationFn: noteID => noteAPI.delete(noteID),
+    onSuccess: (_, noteID) => {
+      if (!document.startViewTransition) {
+        queryClient.invalidateQueries({ queryKey: ['notes'] })
+        return
+      }
+      
+      const listItemEl = document.querySelector(`#note-${noteID}`)
+      listItemEl.classList.remove('incoming')
+      listItemEl.style.viewTransitionName = 'outgoing'
+
+      const transition = document.startViewTransition()
+      if (transition.finished) {
+        queryClient.invalidateQueries({ queryKey: ['notes'] })
+      }
+    }
   })
 }
