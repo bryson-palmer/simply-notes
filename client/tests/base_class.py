3cba715dd0662b27fe03c8376df4ee37657c1037
Lance Kindle <lance.kindle@gmail.com>
2023-09-11 20:40:51 -0700

    rename pausing variables during tests

--- client/tests/base_class.py
+++ client/tests/base_class.py
@@ -14,15 +14,13 @@ class PauseFox(webdriver.Firefox):
       waiting for element to show up before failing to find it
   '''
   def __init__(self, *args, **kwargs):
-    self.retry_time = kwargs.pop('retry_for', None)
-    # defines retry-time for each command (before a failure to find an element throws an exception)
-    if self.retry_time is None:
-      self.retry_time = os.environ.get('RETRY_FOR', 1)
-    # defines pause-time between each command
-    self.pause_between_time = os.environ.get('PAUSE_FOR', 1)
-    self.click_pause_time = os.environ.get('CLICK_PAUSE', 0)  # pause after clicking anything
-    self.should_highlight_element = os.environ.get('HIGHLIGHT', 0)
-    self._element_highlight_tasks = set()  # for preventing un-highlight task from being garbage-collected
+    # defines wait_time for each command (before a failure to find an element throws an exception)
+    if self.wait_time is None:
+      self.wait_time = float(os.environ.get('WAIT', 1))
+    self.pause = float(os.environ.get('PAUSE', 0))  # pause between each action
+    self.after_click_pause = float(os.environ.get('AFTER_CLICK', 0))  # pause after clicking anything
+    self.before_click_pause = float(os.environ.get('BEFORE_CLICK', 0))  # pause BEFORE clicking anything
+    self.should_highlight_element = bool(os.environ.get('HIGHLIGHT', 0))
     super().__init__(*args, **kwargs)
   
   def get(self, *args, **kwargs):
@@ -56,9 +54,9 @@ class PauseFox(webdriver.Firefox):
     return val
 
   def find_element(self, by, selector):
-    time.sleep(self.pause_between_time/2)
+    time.sleep(self.pause/2)
     start_time = time.time()
-    while time.time() - start_time < self.retry_time:
+    while time.time() - start_time < self.wait_time:
       try:
         element = super().find_element(by, selector)
         assert element.is_displayed() and element.is_enabled()
@@ -67,7 +65,7 @@ class PauseFox(webdriver.Firefox):
         continue  # keep trying until timeout
     if self.should_highlight_element:
       self.highlight_element(element)
-    time.sleep(self.pause_between_time/2)
+    time.sleep(self.pause/2)
     return element
 
   def highlight_element(self, element):
