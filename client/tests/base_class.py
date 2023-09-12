4354fda68286c3f014c1a619d9992cfea1556de9
Lance Kindle <lance.kindle@gmail.com>
2023-09-11 22:07:54 -0700

    rewrite webdriver, add refetch stale elements

--- client/tests/base_class.py
+++ client/tests/base_class.py
@@ -3,27 +3,33 @@ import sys
 import os
 import subprocess
 import asyncio
+from typing import Optional
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common import StaleElementReferenceException
 
-class PauseFox(webdriver.Firefox):
-  ''' browser that pauses up to 1 second (configurable)
-      waiting for element to show up before failing to find it
-  '''
+       
+class InitVariablesDriver(webdriver.Firefox):
+
   def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
     # defines wait_time for each command (before a failure to find an element throws an exception)
-    if self.wait_time is None:
-      self.wait_time = float(os.environ.get('WAIT', 1))
+    self.wait_time = float(os.environ.get('WAIT', 1))  # this is time waiting for element to be ready
     self.pause = float(os.environ.get('PAUSE', 0))  # pause between each action
     self.after_click_pause = float(os.environ.get('AFTER_CLICK', 0))  # pause after clicking anything
     self.before_click_pause = float(os.environ.get('BEFORE_CLICK', 0))  # pause BEFORE clicking anything
-    self.should_highlight_element = bool(os.environ.get('HIGHLIGHT', 0))
-    super().__init__(*args, **kwargs)
-  
+
+
+class HighlightElementDriver(InitVariablesDriver):
+  should_highlight_element = False
+
   def get(self, *args, **kwargs):
+    ''' getting web page, add CSS for highlighting elements '''
     val = super().get(*args, **kwargs)
     if not self.should_highlight_element:
       return val
@@ -52,9 +58,23 @@ class PauseFox(webdriver.Firefox):
     # add this animation style to website, that allows briefly highlighting element
     self.execute_script(script)
     return val
+  
+  def find_element(self, by=By.ID, value: str | None = None) -> WebElement:
+    element = super().find_element(by, value)
+    if self.should_highlight_element:
+      self.highlight_element(element)
+    return element
+  
+  def highlight_element(self, element):
+    self.execute_script("arguments[0].classList.add('flash-briefly');", element)
+
+
+class Wait4ElementDriver(HighlightElementDriver):
+  ''' browser that waits up to 1 second (configurable)
+      waiting for element to show up before failing to find it
+  '''
 
   def find_element(self, by, selector):
-    time.sleep(self.pause/2)
     start_time = time.time()
     while time.time() - start_time < self.wait_time:
       try:
@@ -63,13 +83,46 @@ class PauseFox(webdriver.Firefox):
         break  # element is ready!
       except:
         continue  # keep trying until timeout
-    if self.should_highlight_element:
-      self.highlight_element(element)
-    time.sleep(self.pause/2)
     return element
 
-  def highlight_element(self, element):
-    self.execute_script("arguments[0].classList.add('flash-briefly');", element)
+
+class RefetchStaleElement(WebElement):
+  ''' this class should override any method that has triggered a StaleElementException
+      or any other exception that could've been solved by retry-fetching the element first
+      before performing the action.
+  '''
+  _find_by_args = None
+
+  def click(self):
+    time_begin = time.time()
+    # self._parent is the webdriver
+    element = super()
+    val = None
+    while time.time() - time_begin < self._parent.wait_time:
+      try:
+        val = element.click()
+        break
+      except StaleElementReferenceException:
+        # element failed because it became outdated before it got clicked. Generally, refetching
+        # the element and re-clicking is the best course of action. So let's do that!
+        # revert elements to base element (so we don't fall into this function from THAT element)
+        # and ask webdriver parent to find element. Then try clicking it again!
+        self._parent._web_element_cls = super().__class__  # any found element will be base WebElement instance
+        element = self._parent.find_element(*self._find_by_args)
+        continue
+    self._parent._web_element_cls = self.__class__  # restore element found to be this instance again
+    return val
+
+
+class RefetchStaleDriver(Wait4ElementDriver):
+  ''' uses RefetchStaleElement to provide refetching of Element, if when clicking, it is stale '''
+
+  _web_element_cls = RefetchStaleElement # override element to be our wrapper that can retry fetching itself!
+
+  def find_element(self, *args):
+    element = super().find_element(*args)
+    element._find_by_args = args  # for refetching element, should it become stale
+    return element
 
 
 class BrowserSetup():
@@ -102,7 +155,7 @@ class BrowserSetup():
       except subprocess.CalledProcessError as e:
           pass
 
-    self.driver = PauseFox(options=options, service=service)
+    self.driver = RefetchStaleDriver(options=options, service=service)  # it's Firefox improved
     # options = webdriver.ChromeOptions()
     # options.add_argument("--headless")  # Enable headless mode
     # self.driver = webdriver.Chrome(options=options)
