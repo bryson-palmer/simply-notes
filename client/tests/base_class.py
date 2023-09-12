28c6cab999f1ea66891f6ed28bc48cd6eea3b72e
Lance Kindle <lance.kindle@gmail.com>
2023-09-11 17:31:39 -0700

    highlight elements in tests
    elements that are found will be flash / highlight. Mostly are found
    to click, or enter text in
    running yarn test will automatically include highlighting
    which is done by setting HIGHLIGHT=1 in the environment args

--- client/tests/base_class.py
+++ client/tests/base_class.py
@@ -1,6 +1,8 @@
 import time
 import sys
+import os
 import subprocess
+import asyncio
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
@@ -12,18 +14,65 @@ class PauseFox(webdriver.Firefox):
       waiting for element to show up before failing to find it
   '''
   def __init__(self, *args, **kwargs):
-    self.retry_time = kwargs.pop('retry_for', 1)
+    self.retry_time = kwargs.pop('retry_for', None)
+    # defines retry-time for each command (before a failure to find an element throws an exception)
+    if self.retry_time is None:
+      self.retry_time = os.environ.get('RETRY_FOR', 1)
+    # defines pause-time between each command
+    self.pause_between_time = os.environ.get('PAUSE_FOR', 1)
+    self.click_pause_time = os.environ.get('CLICK_PAUSE', 0)  # pause after clicking anything
+    self.should_highlight_element = os.environ.get('HIGHLIGHT', 0)
+    self._element_highlight_tasks = set()  # for preventing un-highlight task from being garbage-collected
     super().__init__(*args, **kwargs)
+  
+  def get(self, *args, **kwargs):
+    val = super().get(*args, **kwargs)
+    if not self.should_highlight_element:
+      return val
+    time.sleep(1)  # waiting for webpage to load... TODO figure out better approach
+    css = """
+    @keyframes briefLightening {
+      0%   { filter: brightness(0); }   /* Start at dark */
+      15%   { filter: brightness(2); }   /* bright up extra */
+      30%  { filter: brightness(0); } /* dark */
+      45%  { filter: brightness(2); } /* bright */
+      60% { filter: brightness(0.5); }   /* darkish */
+      75%  { filter: brightness(1.5); } /* bright-ish */
+      90% { filter: brightness(1); }   /* standard */
+      100% { filter: brightness(1); }   /* standard */
+    }
+    .flash-briefly {
+        animation: briefLightening 1s forwards; /* Adjust 1s to change the total duration */
+    }
+    """
+    script = f"""
+    let style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = `{css}`;
+    document.head.appendChild(style);
+    """
+    # add this animation style to website, that allows briefly highlighting element
+    self.execute_script(script)
+    return val
 
   def find_element(self, by, selector):
+    time.sleep(self.pause_between_time/2)
     start_time = time.time()
     while time.time() - start_time < self.retry_time:
       try:
         element = super().find_element(by, selector)
         assert element.is_displayed() and element.is_enabled()
+        break  # element is ready!
       except:
         continue  # keep trying until timeout
+    if self.should_highlight_element:
+      self.highlight_element(element)
+    time.sleep(self.pause_between_time/2)
     return element
+
+  def highlight_element(self, element):
+    self.execute_script("arguments[0].classList.add('flash-briefly');", element)
+
 
 class BrowserSetup():
   ''' inherit from this base in your selenium tests. Will set up browser with working config
