62ddaa9a9c4bcc2d365989943a0c0ba7301405fc
Bryson Palmer <brysonpalmer@gmail.com>
2023-09-13 13:43:53 -0700

    When getting all notes, build a query key for each note in cache.

--- client/src/hooks/useGetNotes.js
+++ client/src/hooks/useGetNotes.js
@@ -1,15 +1,22 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 import { noteAPI } from '@/apis/noteAPI'
 import { useStore } from '@/store/store'
 
 export default function useGetNotes() {
+  const queryClient = useQueryClient()
   const selectedFolderID = useStore(store => store.selectedFolderID)
   
   return useQuery({
     queryKey: ['notes', selectedFolderID],
     queryFn: () => noteAPI.getAll(selectedFolderID),
     enabled: Boolean(selectedFolderID),
+    onSuccess: (notes) => {
+      console.log('notes', notes)
+      notes.forEach(note => {
+        queryClient.setQueryData(['notes', note.folder, note.id], note)
+      })
+    }
     // keepPreviousData: true,
   })
 }
