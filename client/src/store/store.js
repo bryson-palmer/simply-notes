10695dd7f832ca7a41f6686e9bd541d4f471eb76
Bryson Palmer <brysonpalmer@gmail.com>
2023-09-13 13:11:06 -0700

    Converting zustand store into atomic slices.

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -26,34 +26,38 @@ export const useStore = create(
   persist(
     (set) => ({
       currentNote: null,
-      setCurrentNote: (note) => set(() => ({ currentNote: { ...note } })),
       isNewNote: false,
-      setIsNewNote: (bool) => set(() => ({ isNewNote: bool })),
+      newNoteID: null,
       selectedFolderID: null,
-      setSelectedFolderID: (folderID) =>
-        set(() => ({ selectedFolderID: folderID })),
       selectedNoteID: null,
-      setSelectedNoteID: (id) => set(() => ({ selectedNoteID: id })),
       noteByFolderID: {},
-      /*
-        we're slightly abusing a setter here; we actually update store.noteByFolderID, then return it
-        to be store via the setter
-      */
-      setNoteByFolderID: (
-        folderID = null,
-        noteID = null,
-        removeFolderKey = false
-      ) =>
-        set((store) => {
-          if (removeFolderKey) {
-            // When removeFolderKey is true, delete the folder property from the lookup
-            delete store.noteByFolderID[folderID];
-          } else {
-            // Set folderID key with the value of the noteID
-            store.noteByFolderID[folderID] = noteID;
-          }
-          return store.noteByFolderID;
-        }),
+      actions: {
+        setCurrentNote: (note) => set(() => ({ currentNote: { ...note } })),
+        setIsNewNote: (bool) => set(() => ({ isNewNote: bool })),
+        setNewNoteID: (id) => set(() => ({ newNoteID: id })),
+        setSelectedFolderID: (folderID) =>
+          set(() => ({ selectedFolderID: folderID })),
+        setSelectedNoteID: (id) => set(() => ({ selectedNoteID: id })),
+        /*
+          we're slightly abusing a setter here; we actually update store.noteByFolderID, then return it
+          to be store via the setter
+        */
+        setNoteByFolderID: (
+          folderID = null,
+          noteID = null,
+          removeFolderKey = false
+        ) =>
+          set((store) => {
+            if (removeFolderKey) {
+              // When removeFolderKey is true, delete the folder property from the lookup
+              delete store.noteByFolderID[folderID];
+            } else {
+              // Set folderID key with the value of the noteID
+              store.noteByFolderID[folderID] = noteID;
+            }
+            return store.noteByFolderID;
+          }),
+      },
     }),
     {
       // Local Storage key name
@@ -61,3 +65,11 @@ export const useStore = create(
     }
   )
 )
+
+export const useCurrentNote = () => useStore(state => state.currentNote)
+export const useIsNewNote = () => useStore(state => state.isNewNote)
+export const useNewNoteID = () => useStore(state => state.newNoteID)
+export const useSelectedFolderID = () => useStore(state => state.selectedFolderID)
+export const useSelectedNoteID = () => useStore(state => state.selectedNoteID)
+export const useNoteByFolderID = () => useStore(state => state.noteByFolderID)
+export const useStoreActions = () => useStore(state => state.actions)
