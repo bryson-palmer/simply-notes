10aad742adc896e056b6a219fa56bc5cc17eedaf
Lance Kindle <lance.kindle@gmail.com>
2023-09-14 13:23:47 -0700

    add backend note test

--- server/test_backend.py
+++ server/test_backend.py
@@ -3,6 +3,9 @@
 import pytest
 from bs4 import BeautifulSoup
 from app import app
+import notes as Notes
+
+USER = 'USER_ID'
 
 @pytest.fixture
 def client():
@@ -10,6 +13,21 @@ def client():
     app.testing = True
     with app.test_client() as client:
         yield client
+
+def delete_all_notes():
+    notes = Notes.get_notes('', USER)
+    print(notes)
+    note_ids = [note['id'] for note in notes]
+    Notes.delete_notes(note_ids, USER)
+
+@pytest.fixture(autouse=True)
+def setup_and_teardown():
+    ''' pytest fixture abuse that's well accepted. Used to perform setup & teardown for each test '''
+    # do set up stuff here
+    delete_all_notes()
+    yield
+    # do teardown stuff here
+
 
 def test_home_endpoint_looks_like_html(client):
     ''' home endpoint returns html that loads the front-end code '''
@@ -19,4 +37,20 @@ def test_home_endpoint_looks_like_html(client):
     soup = BeautifulSoup(html_text, 'html.parser')
     assert soup.find('html') or soup.find('body')
     #assert response.get_json() == {"message": "Hello, World!"}
+
+
+def test_create_and_get_note():
+    # assert there are no note currently or this user
+    assert [] == Notes.get_notes('', USER)
+    note = dict(id='1234', title='title text', body='body text', folder='unknown folder')
+    Notes.create_or_modify_note(note, user_id=USER)
+    saved_note = Notes.get_note(note['id'], USER)
+    for k, value in note.items():
+        assert value == saved_note[k]
+    # now just verify it also works when retrieving from all notes
+    all_notes = Notes.get_notes('', USER)
+    saved_note = all_notes[0]
+    for k, value in note.items():
+        assert value == saved_note[k]
+
 
